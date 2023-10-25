aded6f8e0cee1119a98a6e175a092d58ec1c1914
Subha <subharamkie@gmail.com>
2023-10-25 20:57:11 +0100

    Refator:sorted array before finding frequency,used Object.entries to sort the object

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,52 +1,21 @@
-var txr = [];
+function processTransactions(transactions) {
+  if (!transactions) {
+    throw new Error("Undefined collection of transactions");
+  }
+  let transactionArray = [];
+  let transactionObjCount = {};
 
-function processTransactions(transActions) {
+  transactions.sort();
+  transactions.forEach((item) => {
+    transactionObjCount[item] = (transactionObjCount[item] || 0) + 1;
+  });
+  transactionArray = Object.entries(transactionObjCount)
+    .sort(([, a], [, b]) => {
+      return b - a;
+    })
+    .map((item) => `${item[0]} ${item[1]}`);
 
-    txr = [];
-
-    if(!validateTransactions(transActions)) {
-        throw new Error("Undefined collection of transactions")
-    }
-
-    let txCount = {}
-
-    const numberOfTransactions = transActions.length;
-
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
-
-    txCount = sortByAmountThenName(txCount);
-    
-    // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
-
-    return txr;
-}
-
-function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
-
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
-    return sortedResults;
-}
-
-
-function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
-
-    return true;
+  return transactionArray;
 }
 
 module.exports = processTransactions;
